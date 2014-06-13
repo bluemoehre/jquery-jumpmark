e2b86c863a2569154e113f544e832f3df29bf23a
Möhre <blue_moehre@gmx.de>
2014-06-13 20:57:46 +0200

    tons of improvements

--- jquery.jumpmark.js
+++ jquery.jumpmark.js
@@ -4,8 +4,6 @@
  * @copyright 2013 BlueMöhre
  * @link http://www.github.com/bluemoehre
  */
-
-// use window as local variable due to performance improvement
 (function($, win, doc) {
 
     'use strict';
@@ -17,45 +15,38 @@
     var PLUGIN_NAME = 'jumpmark';
 
     /**
-     * @type {string}
+     * The plugin options
+     * @type {{hashPrefix: string, clipOffset: (number|string), hopOffset: (number|string)}}
      */
-    var ANCHOR_PREFIX = 'jump:';
-
-    /**
-     * Defines a general offset between the target element and the viewport's top
-     * - If set to zero the target element will clip at the top of the viewport (if possible)
-     * - If set higher than zero the there will be some distance between viewport's top and element (more natural)
-     * - You may add '%' to use a relative offset related to the viewport else it will be treated as pixels
-     * @type {(number|string)}
-     */
-    var CLIP_OFFSET = '15%';
-
-    /**
-     * Is TRUE for a relative clip offset and FALSE for an absolute value
-     * @type {boolean}
-     */
-    var CLIP_OFFSET_RELATIVE = CLIP_OFFSET.substr(-1) == '%';
-
-    /**
-     * If a jump mark was present on page load this value is used to hop right before the jump mark and start animation there.
-     * So we wont scroll over the whole document which might be quite annoying to the user.
-     * - If this is zero no animation will happen
-     * - You may add '%' to use a relative offset related to the viewport else it will be treated as pixels
-     * @type {(number|string)}
-     */
-    var HOP_OFFSET = '10%';
-
-    /**
-     * Is TRUE for a relative hop offset and FALSE for an absolute value
-     * @type {boolean}
-     */
-    var HOP_OFFSET_RELATIVE = HOP_OFFSET.substr(-1) == '%';
+    var opts = {
+        /**
+         * Defines a prefix which must be present for using animation by this plugin.
+         * If no prefix is given, jumping to all anchors will be animated.
+         * (This is only for the address bar and links. Please do NOT add this to your target IDs.)
+         */
+        hashPrefix: 'jump:',
+        /**
+         * Defines a general offset between the target element and the viewport's top
+         * - A number will be treated as pixels. You may add '%' to use a relative offset related to the viewport.
+         * - If set to zero the target element will clip at the top of the viewport (if possible)
+         * - If set higher than zero the there will be some distance between viewport's top and the element (more natural)
+         */
+        clipOffset: '15%',
+        /**
+         * If a jump mark was present on page load, hop right before the target and start scrolling animation from there.
+         * That prevents scrolling over the whole document, which might be quite annoying to the user.
+         * - A number will be treated as pixels. You may add '%' to use a relative offset related to the viewport.
+         * - If this is zero no animation will happen
+         */
+        hopOffset: '15%',
+        animSpeed: 750
+    };
 
     /**
      * Holds the jQuery object of the scrollable container
      * Due to differences in Mozilla and Webkit this must be html & body
      * ATTENTION! Maybe at this point of execution the DOM contains no BODY, so select it with the READY-Event later
-     * @type {(jQuery|null)}
+     * @type {?jQuery}
      */
     var $viewport = null;
 
@@ -70,108 +61,169 @@
     var $doc = $(doc);
 
     /**
-     * Returns the hop offset respecting the current viewport height
+     * @type {boolean}
      */
-    var calcHopOffset = function(){
-        return HOP_OFFSET_RELATIVE ? $win.height()/100*parseInt(HOP_OFFSET) : parseInt(HOP_OFFSET);
-    };
+    var ignoreHashChangeEvent = false;
+
 
     /**
-     * Returns the clip offset respecting the current viewport height
+     * Returns TRUE for a relative clip offset and FALSE for an absolute value
+     * @type {boolean}
      */
-    var calcClipOffset = function(){
-        return CLIP_OFFSET_RELATIVE ? $win.height()/100*parseInt(CLIP_OFFSET) : parseInt(CLIP_OFFSET);
-    };
+    function isClipOffsetRelative(){
+        return opts.clipOffset.substr(-1) == '%';
+    }
 
     /**
-     * Returns elements top offset respecting the clip offset
-     * @param {Object} el
+     * Returns TRUE for a relative hop offset and FALSE for an absolute value
+     * @type {boolean}
      */
-    var calcOffset = function(el){
+    function isHopOffsetRelative(){
+        return opts.hopOffset.substr(-1) == '%';
+    }
+
+    /**
+     * Returns the hop offset in pixels respecting the current viewport height
+     * @returns {number}
+     */
+    function calcHopOffset(){
+        return isHopOffsetRelative() ? $win.height()/100*parseInt(opts.hopOffset) : parseInt(opts.hopOffset);
+    }
+
+    /**
+     * Returns the clip offset in pixels respecting the current viewport height
+     * @returns {number}
+     */
+    function calcClipOffset(){
+        return isClipOffsetRelative() ? $win.height()/100*parseInt(opts.clipOffset) : parseInt(opts.clipOffset);
+    }
+
+    /**
+     * Returns elements top offset in pixels respecting the clip offset
+     * @param {(HTMLElement|jQuery)} el
+     * @returns {number}
+     */
+    function calcTargetOffset(el){
         var offset = $(el).offset().top - calcClipOffset();
         return offset > 0 ? offset : 0;
-    };
+    }
 
     /**
      * Scrolls to the given target
-     * - If hop is true, we will skip some part of the scrolling to save some time
-     * @param {(number|string)} target
-     * @param {boolean} [hop]
+     * - If doHop is true, we will skip some part of the scrolling to save some time using the opts.hopOffset
+     * @param {(number|string|HTMLElement|jQuery)} target
+     * @param {boolean} [doHop]
      */
-    var scrollTo = function(target, hop){
-        var hopOffset = calcHopOffset();
-        var el;
-        var offset;
-        var distance;
+    function scrollTo(target, doHop){
+        var hopOffset;
+        var elOffset;
+        var diffOffset;
 
-        // if given argument was a number
         if (typeof target === 'number'){
-            offset = target;
+            elOffset = target;
         }
-        // if given argument was a valid anchor ID
-        else if (target.match(new RegExp('^' + ANCHOR_PREFIX))) {
-            el = doc.getElementById(target);
-
-            // if there is an element with the given anchor ID calculate its offset
+        // if there is an element with the given anchor ID calculate its offset
+        // else test for special target
+        else if (typeof target === 'string' && target.indexOf('#' + opts.hashPrefix) == 0){
+            target = target.replace('#' + opts.hashPrefix, '');
+            var el = doc.getElementById(target);
             if (el){
-                offset = calcOffset(el);
+                elOffset = calcTargetOffset(el);
+            } else if (target == 'top'){
+                elOffset = 0;
+            } else if (target == 'bottom'){
+                elOffset = $win.height();
+            } else {
+                return;
             }
-            // if there is no element with the given anchor ID but the anchor was page-top, simply set offset to zero
-            else if (target == ANCHOR_PREFIX +'_top'){
-                offset = 0;
-            }
+        } else {
+            elOffset = calcTargetOffset(target);
         }
 
-        // scroll to offset
-        if (typeof offset !== 'undefined'){
-            distance = offset - $viewport.scrollTop();
+        if (elOffset !== undefined && elOffset !== null){
+            hopOffset = calcHopOffset();
+            diffOffset = elOffset - $win.scrollTop();
+
             // do hopping only if wanted and necessary
-            if (hop && hopOffset < Math.abs(distance)){
-                // care about the  target might be above current visible page part
-                win.scrollTo(win.pageXOffset, offset - (hopOffset * (distance > 0 ? 1 : -1)));
+            if (doHop && hopOffset < Math.abs(diffOffset)){
+                // care about the target might be above current visible page part
+                if (diffOffset > 0){
+                    $viewport.scrollTop(elOffset - hopOffset);
+                } else {
+                    $viewport.scrollTop(elOffset + hopOffset);
+                }
             }
-            $viewport.stop(true).animate({
-                scrollTop: offset
-            });
-        }
-    };
 
-    // on document ready select the viewport
-    // if the location hash contains valid jump mark and scroll there
+            $viewport.stop(true).animate({
+                scrollTop: elOffset
+            }, opts.animSpeed);
+        }
+    }
+
+    // if the location hash contains valid jump mark when document is ready scroll to it
     $(function(){
         $viewport = $('html, body');
-        if (win.location.hash.match(new RegExp('^#\\!?'+ ANCHOR_PREFIX))){
-            scrollTo(win.location.hash.replace(/#!?/,''), true);
+        if (win.location.hash.indexOf('#' + opts.hashPrefix) == 0){
+            scrollTo(win.location.hash, true);
         }
     });
 
-    // automatically handle all clicks on jump marks
-    $doc.on('click.' + PLUGIN_NAME, 'a[href^="#'+ ANCHOR_PREFIX +'"]', function(evt){
-        evt.preventDefault();
-        var stateObj = win.history.state || {};
-        if (!win.location.hash){
-            stateObj[PLUGIN_NAME + 'Target'] = $win.scrollTop();
-            win.history.replaceState(stateObj);
+    // automatically handle all clicks on links with hashes/jump marks
+    $doc.on('click.' + PLUGIN_NAME, 'a[href^="#' + opts.hashPrefix + '"]', function(evt){
+        if (!evt.isDefaultPrevented()){
+            evt.preventDefault();
+            var stateObj = win.history.state || {};
+
+            // save current state
+            if (!win.location.hash){
+                stateObj[PLUGIN_NAME] = $win.scrollTop();
+                win.history.replaceState(stateObj);
+            }
+
+            stateObj[PLUGIN_NAME] = this.hash;
+            win.history.pushState(stateObj, null, this.href);
+            scrollTo(this.hash, false);
         }
-        stateObj = {};
-        stateObj[PLUGIN_NAME + 'Target'] = this.hash.slice(1);
-        // add exclamation mark as little hack to prevent tha browsers native scrolling on popstate
-        win.history.pushState(stateObj, null, this.hash.replace('#','#!'));
-        scrollTo(stateObj[PLUGIN_NAME + 'Target'], false);
     });
 
     // automatically handle history navigation
     $win.on('popstate.' + PLUGIN_NAME, function(evt){
-        if(evt.originalEvent.state && PLUGIN_NAME + 'Target' in evt.originalEvent.state){
-            scrollTo(evt.originalEvent.state[PLUGIN_NAME + 'Target'], false);
+        if(evt.originalEvent.state && evt.originalEvent.state[PLUGIN_NAME] !== undefined){
+            // prevent native scrolling - atm no effect =/
+            evt.preventDefault();
+            scrollTo(evt.originalEvent.state[PLUGIN_NAME], false);
+            ignoreHashChangeEvent = true;
+            setTimeout(function(){
+                ignoreHashChangeEvent = false;
+            }, opts.animSpeed);
         }
     });
 
     // automatically handle hash change by user or JavaScript
-    $win.on('hashchange.' + PLUGIN_NAME, function(){
-        if (win.location.hash.match(new RegExp('^#\\!?'+ ANCHOR_PREFIX))){
-            scrollTo(win.location.hash.replace(/#!?/,''), false);
+    $win.on('hashchange.' + PLUGIN_NAME, function(evt){
+        if (win.location.hash.indexOf('#' + opts.hashPrefix) == 0 && !ignoreHashChangeEvent){
+            evt.preventDefault();
+            var stateObj = {};
+            stateObj[PLUGIN_NAME] = win.location.hash;
+            win.history.replaceState(stateObj);
+            scrollTo(win.location.hash, false);
         }
     });
+
+    // abort animation on user scroll
+    $win.on('DOMMouseScroll.' + PLUGIN_NAME +' mousewheel.' + PLUGIN_NAME, function(){
+        $viewport.stop(true);
+    });
+
+    // register plugin to jQuery so the options are changeable
+    $[PLUGIN_NAME] = function(arg){
+        if (typeof arg === 'string' || arg instanceof jQuery || arg instanceof HTMLElement){
+            scrollTo(arg);
+        } else if (typeof arg === 'object' && arg.toString() == '[object Object]'){
+            opts = $.extend(opts, arg);
+        } else  {
+            $.error('parameter must be String, HTMLElement, jQuery or configuration Object');
+        }
+    };
 
 })(jQuery, window, document);
